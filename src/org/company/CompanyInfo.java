3941f84af400b517a4d927c8d6948093c9a5a177
PrakashRadhakrishnan7 <Prakeshraakii@gmail.com>
2023-10-06 15:46:38 +0530

    newtest1

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,26 @@
+package org.company;
+
+public class CompanyInfo {
+	private void name() {
+System.out.println("tata");
+	}
+	private void id() {
+		System.out.println("abc123");
+}
+	private void address() {
+		System.out.println("chennai28");
+}
+	public static void main(String[] args) {
+		CompanyInfo c =new CompanyInfo ();
+		c.name();
+		c.id();
+		c.address();
+		
+		
+	}
+
+	
+	
+	}
+
+
